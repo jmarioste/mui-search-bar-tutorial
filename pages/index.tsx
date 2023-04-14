cad055e83825e98b1cfe25760c78972bf4b790b2
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-04-14 17:33:22 +0800

    Adding Props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,26 @@ import type { NextPage } from "next";
 
 import { Box, Typography } from "@mui/material";
 import Searchbar from "../src/components/Searchbar";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
     <Box maxWidth={"sm"} margin="auto">
       <Typography textAlign="center" my={2}>
         MUI <code>{`<SearchBar/>`}</code> Tutorial
       </Typography>
       <Box my={10}>
-        <Searchbar />
+        <Searchbar
+          onSubmit={(searchTerm: string) => {
+            router.push({
+              query: {
+                search: searchTerm,
+              },
+            });
+          }}
+        />
+        <code>{router.asPath}</code>
       </Box>
     </Box>
   );
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,18 +1,37 @@
 import { SearchOutlined } from "@mui/icons-material";
-import { Divider, IconButton, InputBase, Paper } from "@mui/material";
+import {
+  Divider,
+  IconButton,
+  InputBase,
+  InputBaseProps,
+  Paper,
+} from "@mui/material";
+import { useState } from "react";
 
-const Searchbar = () => {
+type Props = {
+  onSubmit(searchTerm: string): void;
+  onFocus?(): void;
+};
+const Searchbar = (props: Props) => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <Paper
       component="form"
       elevation={3}
       sx={{ display: "flex", alignItems: "center", px: 1, py: 0.5 }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.onSubmit((searchTerm as string) ?? "");
+      }}
     >
       {/* Input base contains the fewest styles possible so it's perfect for creating custom components like these */}
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search..."
         inputProps={{ "aria-label": "search" }}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onFocus={(e) => props.onFocus && props.onFocus()}
       />
       <Divider sx={{ height: 28, mx: 0.5 }} orientation="vertical" />
       <IconButton>
